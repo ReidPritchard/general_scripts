cb4dd9f6e06c3e5ba93f20492bf0979a4341c8ef
Reid <reidpritchard99@gmail.com>
2020-06-22 19:54:29 -0400

    smarter join char, smarter divide for single line

--- divide_and_join_text.js
+++ divide_and_join_text.js
@@ -24,10 +24,54 @@ if (doc) {
 }
 
 function divide(selected) {
-	//get object position
-	var selected_width = selected.width;
-	if (selected.contents.indexOf("\n") != -1) {
-		//alert("This IS already a single line object!");
+
+	if (selected.contents.indexOf("\n") === -1) {
+		// Since this is one line we will assume the user is trying to split by word rather than line
+
+		// Generate a array of words
+		var array_of_words = get_tf_contents(selected.words);
+
+		// Save left position of text field to later position the new words
+		textfield_left = selected.left;
+
+		// Set selected element to value of ' '
+		selected.contents = ' ';
+		// Find the width of one character 
+		var char_width = selected.width;
+
+		// Set selected element to value of first word 
+		selected.contents = array_of_words[0];
+		// Convert it to a point object, for no reason other than they are nicer for single words
+		selected.convertAreaObjectToPointObject();
+		// Deselect text field
+		selected.selected = false;
+
+		// Start offset as first word width + the width of a char (or space)
+		var offset = selected.width + char_width;
+
+		// Init new text field to be used in for loop
+		var new_text_field;
+
+		for (var j = 1; j < array_of_words.length; j++) {
+			// Duplicate text field and place at beginning of first text 
+			new_text_field = selected.duplicate(doc, ElementPlacement.PLACEATBEGINNING);
+
+			// Set contents of this new text field to the next word in the array
+			new_text_field.contents = array_of_words[j];
+
+			// Set left position of text field by offset 
+			new_text_field.left = textfield_left + offset;
+
+			// Update offset based on text_field width with extra char
+			offset += new_text_field.width + char_width;
+
+			// Deselect new text field
+			new_text_field.selected = false;
+
+			// Convert it to a point object, for no reason other than they are nicer for single words
+			new_text_field.convertAreaObjectToPointObject();
+		}
+		
 	} else {
 		//getObject justification
 		var justification = selected.story.textRange.justification;
@@ -43,12 +87,17 @@ function divide(selected) {
 
 		//for each array item, create a new text line
 		var tr = selected.story.textRange;
-		var vSpacing = tr.leading;
+		var leading = tr.leading;
 		var newTF;
 		for (j = 1; j < array_of_lines.length; j++) {
 			newTF = selected.duplicate(doc, ElementPlacement.PLACEATBEGINNING);
+
+			// Set contents of this new text field to the next line in the array
 			newTF.contents = array_of_lines[j];
-			newTF.top = textfield_top - vSpacing * j;
+
+			// Get top position of text field and offset by leading amount
+			newTF.top = textfield_top - leading * j;
+
 			if (justification == Justification.CENTER) {
 				newTF.left = textfield_left + selected.width / 2 - newTF.width / 2;
 			} else if (justification == Justification.RIGHT) {
@@ -56,6 +105,8 @@ function divide(selected) {
 			} else {
 				newTF.left = textfield_left;
 			}
+
+			// Deselect new text field
 			newTF.selected = false;
 			newTF.convertAreaObjectToPointObject();
 		}
@@ -73,12 +124,36 @@ function get_line_content(selection){
 
 function join(selections) {
 	selections = selections.reverse();
-	selections[0].contents = get_tf_contents(selections).join("\n");
+	selections[0].contents = get_tf_contents(selections).join(pick_join_char(selections));
 	selections[0].convertPointObjectToAreaObject();
 
 	for (var i = 1; i < selections.length; i++){
 		selections[i].remove();
 	}
+}
+
+function pick_join_char(selections) {
+	var min_width = selections[0].left;
+	var max_width = selections[0].left;
+
+	var max_height = selections[0].top;
+	var min_height = selections[0].top;
+	for (var i = 1; i < selections.length; i++) {
+		max_width = Math.max(max_width, selections[i].left);
+		min_width = Math.min(min_width, selections[i].left);
+
+		max_height = Math.max(max_height, selections[i].top);
+		min_height = Math.min(min_height, selections[i].top);
+	}
+
+	// alert(max_width+ ' '+ min_width+ ' : '+ max_height+' '+ min_height);
+
+	if (Math.abs(max_width - min_width) > Math.abs(max_height - min_height)) {
+		return ' ';
+	}else{
+		return '\n';
+	}
+
 }
 
 function get_tf_contents(selection){
@@ -92,3 +167,4 @@ function get_tf_contents(selection){
 function trim(s){
   return ( s || '' ).replace( /^\s+|\s+$/g, '' );
 }
+
